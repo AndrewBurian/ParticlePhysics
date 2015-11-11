6377b952fef9d315e4506f96b44eac3880331ea9
Andrew Burian <andrew@burian.ca>
2015-11-11 13:33:06 -0800

    Physics force calculation added
    Equations ported directly from v1 of the simulator.
    Slightly revamped logic to increase efficiency.

--- physics.c
+++ physics.c
@@ -1,17 +1,77 @@
 #include "physics.h"
+#include "universe.h"
+#include <math.h>
+
+void applyForcePart(const double constant, struct particle*, struct particle*);
 
 void physicsApply(struct universe *univ) {
+	applyGravity(univ);
+	applyElectric(univ);
+	applyMovement(univ);
+	applyCollision(univ);
+}
+
+void applyGravity(struct universe* univ) {
+	int i, j = 0;
+
+	for (i = 0; i < univ->highestParticle; i++) {
+		if (!univ->particles[i].isActive) {
+			continue;
+		}
+
+		for (j = i; j < univ->highestParticle; j++) {
+			if(!univ->particles[j].isActive) {
+				continue;
+			}
+
+			applyForcePart(GRAVITIATION, &univ->particles[i], &univ->particles[j]);
+
+		}
+
+	}
+}
+
+void applyElectric(struct universe* univ) {
+
+	int i, j = 0;
+
+	for (i = 0; i < univ->highestParticle; i++) {
+		if (!univ->particles[i].isActive) {
+			continue;
+		}
+
+		for (j = i; j < univ->highestParticle; j++) {
+			if(!univ->particles[j].isActive) {
+				continue;
+			}
+
+			applyForcePart(ELECTROSTATIC, &univ->particles[i], &univ->particles[j]);
+
+		}
+
+	}
+}
+
+void applyForcePart(const double constant, struct particle* a, struct particle* b) {
+
+	double xDist = a->xPos - b->xPos;
+	double yDist = a->yPos - b->yPos;
+	double distance = sqrt(pow(xDist, 2) + pow(yDist, 2));
+
+	double totalForce = constant * ((a->mass * b->mass) / pow(distance, 2));
+
+	a->xForce -= totalForce * xDist / distance;
+	a->yForce -= totalForce * yDist / distance;
+
+	b->xForce += totalForce * xDist / distance;
+	b->yForce += totalForce * yDist / distance;
 
 }
 
-void addParticle(struct particle *p) {
+void applyMovement(struct universe* univ) {
 
 }
 
-void deleteParticle(struct particle *p) {
+void applyCollision(struct universe* univ) {
 
-}
-
-struct particle* getParticle(double xPos, double yPos) {
-	return 0;
 }
--- physics.h
+++ physics.h
@@ -3,9 +3,14 @@
 
 #include "universe.h"
 
+const double GRAVITIATION = 6.77e-11L;
+const double ELECTROSTATIC = 8.987e+9L;
+
 void physicsApply(struct universe*);
-void addParticle(struct particle*);
-void deleteParticle(struct particle*);
-struct particle* getParticle(double, double);
+void applyGravity(struct universe*);
+void applyElectric(struct universe*);
+void applyMovement(struct universe*);
+void applyCollision(struct universe*);
+
 
 #endif
