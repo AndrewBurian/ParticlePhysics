773ec45130b9314d436f58d05db969e4a80add6c
Andrew Burian <andrew@burian.ca>
2015-11-11 14:45:30 -0800

    Added collisions
    All collisions are prefectly inelastic and result in two
    particles a and b becoming merged into particle a. b is then
    removed from the universe.

--- physics.c
+++ physics.c
@@ -3,6 +3,7 @@
 #include <math.h>
 
 void applyForcePart(const double constant, struct particle*, struct particle*);
+int collideParticles(struct particle*, struct particle*);
 
 void physicsApply(struct universe *univ) {
 	applyGravity(univ);
@@ -104,4 +105,57 @@ void applyMovement(struct universe* univ) {
 
 void applyCollision(struct universe* univ) {
 
+	int i, j = 0;
+	double distance;
+
+	for (i = 0; i < univ->highestParticle; i++) {
+		if (!univ->particles[i].isActive) {
+			continue;
+		}
+
+		for (j = i; j < univ->highestParticle; j++) {
+			if(!univ->particles[j].isActive) {
+				continue;
+			}
+
+			if (collideParticles(&univ->particles[i], &univ->particles[j])) {
+				deleteParticle(univ, &univ->particles[j]);
+			}
+
+		}
+
+	}
+}
+
+int collideParticles(struct particle* a, struct particle* b) {
+	double distance = sqrt(pow(a->xPos - b->xPos, 2) + pow(a->yPos - b->yPos, 2));
+
+	// no collision check
+	if (distance > a->size + b->size) {
+		return 0;
+	}
+
+	// combine into particle a
+
+	// add charge
+	a->charge += b->charge;
+
+	// mass-weighted new position
+	a->xPos = ((a->xPos * a->mass) + (b->xPos * b->mass)) / (a->mass + b->mass);
+	a->xPos = ((a->yPos * a->mass) + (b->yPos * b->mass)) / (a->mass + b->mass);
+
+	// conservation of momentum
+	a->xVel = ((a->xVel * a->mass) + (b->xVel, b->mass)) / (a->mass + b->mass);
+	a->yVel = ((a->yVel * a->mass) + (b->yVel, b->mass)) / (a->mass + b->mass);
+
+	// consertation of mass
+	a->mass += b->mass;
+
+	// stationary particles remain so
+	a->isStationary += b->isStationary;
+
+	// size is a function of density and mass
+	a->size = sqrt(a->mass / DENSITY / M_PI);
+
+	return 1;
 }
--- physics.h
+++ physics.h
@@ -5,6 +5,7 @@
 
 #define GRAVITIATION 6.77e-11L
 #define ELECTROSTATIC 8.987e+9L
+#define DENSITY 1000 // kg/m^2
 
 void physicsApply(struct universe*);
 void applyGravity(struct universe*);
