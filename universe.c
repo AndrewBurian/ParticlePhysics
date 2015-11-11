d6a788215122c88e26cc82d524bfd6f19ebc1f56
Andrew Burian <andrew@burian.ca>
2015-11-11 15:07:53 -0800

    Added universe expansion
    Universe is realloced by doubling the particle space

--- universe.c
+++ universe.c
@@ -1,25 +1,41 @@
 #include "universe.h"
 #include <string.h>
+#include <stdlib.h>
 
 void addParticle(struct universe *univ, struct particle *p) {
 
 	int i = 0;
 
-	// loop through the universe
-	for (i = 0; i < univ->particleCount; i++) {
+	// continue looping until particle is added
+	while(1) {
 
-		// find an unused particle
-		if(!univ->particles[i].isActive) {
+		// loop through the universe
+		for (i = 0; i < univ->particleCount; i++) {
 
-			// copy the particle into the universe
-			memcpy(&univ->particles[i], p, sizeof(struct particle));
-			univ->particles[i].isActive = 1;
+			// find an unused particle
+			if(!univ->particles[i].isActive) {
 
-			// adjust highest particle if needed
-			if(i > univ->highestParticle) {
-				univ->highestParticle = i;
+				// copy the particle into the universe
+				memcpy(&univ->particles[i], p, sizeof(struct particle));
+				univ->particles[i].isActive = 1;
+
+				// adjust highest particle if needed
+				if(i > univ->highestParticle) {
+					univ->highestParticle = i;
+				}
+
+				// particle added
+				break;
 			}
 		}
+
+		// if the particle was added
+		if (i < univ->particleCount) {
+			break;
+		}
+
+		// otherwise expand the universe and try again
+		universeExpand(univ);
 	}
 }
 
@@ -45,4 +61,18 @@ void deleteParticle(struct universe *univ, struct particle *p) {
 
 struct particle* getParticle(double xPos, double yPos) {
 	return 0;
+}
+
+void universeExpand(struct universe *univ) {
+
+	int prev = univ->particleCount;
+
+	// double particle space
+	univ->particleCount *= 2;
+
+	univ->particles = realloc(univ->particles, univ->particleCount * sizeof(struct particle));
+
+	// zero out new particle space
+	memset(&univ->particles[prev], 0, (univ->particleCount - prev) * sizeof(struct particle));
+
 }
